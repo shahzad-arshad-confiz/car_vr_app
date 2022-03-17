e83f0ff2bc50740c4b7df5b3e48887f515d61c93
shahzadarshad <shahzad.arshad@confiz.com>
2022-03-17 16:48:08 +0500

    initial commit

---
+++ ARSimpleApp/App.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import {StyleSheet} from 'react-native';
+import {
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroARSceneNavigator,
+  ViroNode,
+  ViroSphere,
+  ViroScene,
+
+} from '@viro-community/react-viro';
+
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState('Welcome Confiz guys in AR and VR Session...');
+
+  function onInitialized(state, reason) {
+    console.log('guncelleme', state, reason);
+    if (state === ViroConstants.TRACKING_NORMAL) {
+      setText('Hello Confiz!');
+    } else if (state === ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+      setText('Losing position!');
+    }
+  }
+
+  return (
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
+      <ViroText
+        text={"Hello Sir!"}
+        scale={[0.9, 0.9, 0.9]}
+        position={[0.7, 0.7, -1.8]}
+        style={styles.helloWorldTextStyle}
+      />
+    </ViroARScene>
+  );
+};
+
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: {flex: 1},
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
